473742ff15dc9f660991d915a2105496137e9a8e
khomsunt <khom_meanmon@hotmail.com>
2024-05-14 19:34:18 +0700

    แก้ไข navbar ให้มีสีพื้นหลังเมื่อ hover

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -4,13 +4,12 @@ import Link from 'next/link'
 const Navbar = () => {
     return (
         <nav className='h-12 bg-blue-700'>
-            <div className=" text-white flex justify-center">
-                <div>Log</div>
-                <div>
-                    <Link className='p-3' href="/">Home</Link>
-                    <Link className='p-3' href="/about">About</Link>
-                    <Link className='p-3' href="/map">Map</Link>
-
+            <div className=" text-white flex justify-between pt-3 px-5">
+                <div>Logo</div>
+                <div className='space-x-2'>
+                    <Link className='p-1 px-2 hover:bg-white hover:text-blue-700 rounded-lg' href="/">Home</Link>
+                    <Link className='p-1 px-2 hover:bg-white hover:text-blue-700 rounded-lg' href="/about">About</Link>
+                    <Link className='p-1 px-2 hover:bg-white hover:text-blue-700 rounded-lg' href="/map">Map</Link>
                 </div>
             </div>
 
