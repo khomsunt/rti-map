944d8009decf944193ff5f1193d596371b852e65
sureelux <sureeluxtip2545@gmail.com>
2024-05-14 23:19:45 +0700

    nt

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-center text-3xl'>About</div>
+    <div className="hero-content flex-col lg:flex-row">
+      <img src="https://get.wallhere.com/photo/nature-landscape-1429571.jpg" className="shadow-2xl" />
+      <h1 className='text-5xl font-bold text-center mt-10'>เกี่ยวกับ</h1>
+      <p className='text-3xl font-bold text-center mt-10'>ชอบความธรรมดา ที่มีธรรมชาติล้อมรอบ</p>
+    </div>
   )
 }
 
