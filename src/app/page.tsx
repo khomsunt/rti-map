f8cf482a990cd9588a68bf55b367d65f36878cb4
tphrmmung <tphrmmung@gmail.com>
2024-05-15 12:10:01 +0700

    wet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+const Contact = () => {
   return (
-    <div className="text-center text-3xl">
-      Home Test
+    <div className='h-screen'>
+      <div className="hero min-h-screen bg-base-100 ml-40">
+        <hr />
+        <img src="https://cms.dmpcdn.com/travel/2023/04/14/10feb5a0-da8f-11ed-b50c-375a25fe19c4_webp_original.jpg" className="max-w-full rounded-lg shadow-2xl mt-10 ml-56" style={{ maxWidth: '800px' }}  />
+        <h1 className='first-letter:text-balance text-2xl mt-2 ml-80'>ที่เที่ยวสวยที่สุดในไทย มีที่ไหนบ้าง!! มาเช็คลิสต์กัน</h1>
+        <h1 className='first-letter:text-balance text-2xl mt-2 '> ใน ประเทศไทย บ้านเรามีที่เที่ยวเยอะแยะแบบที่ไปกันได้ทั้งปีทุกวี่ทุกวันก็ยังไม่มีหมดค่ะ 
+        นี่ยังไม่นับรวมแหล่งท่องเที่ยวใหม่ๆ ที่ผุดกันขึ้นมาเรื่อยๆ เรียกได้ว่าเมืองไทยของเรานั้น 
+        แทบจะเป็นสวรรค์ของนักท่องเที่ยวกันเลย ว่าแล้วมาดู 55 ที่เที่ยว สวยที่สุดในไทย ชาตินี้ต้องไปสักครั้ง!!</h1>
+      </div>
     </div>
   );
-}
+};
+
+export default Contact;
