0cd3aff960a26a01cb342f9cd09acafe13f5f97d
sureelux <sureeluxtip2545@gmail.com>
2024-05-15 09:52:35 +0700

    nt

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div className="hero-content flex-col lg:flex-row">
       <img src="https://get.wallhere.com/photo/nature-landscape-1429571.jpg" className="shadow-2xl" />
-      <h1 className='text-5xl font-bold text-center mt-10'>เกี่ยวกับ</h1>
-      <p className='text-3xl font-bold text-center mt-10'>ชอบความธรรมดา ที่มีธรรมชาติล้อมรอบ</p>
+      <h1 className='text-6xl font-bold mt-10 ml-16'>ABOUT</h1>
+      <p className='text-2xl mt-5 ml-16'>ชอบความธรรมดา ที่มีธรรมชาติล้อมรอบ</p>
     </div>
   )
 }
