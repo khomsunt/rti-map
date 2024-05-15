cf3cbc3aea511e63ca883623712a78059fa10549
sureelux <sureeluxtip2545@gmail.com>
2024-05-15 11:10:55 +0700

    nt

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="h-screen">
       <div className="hero-content flex-col lg:flex-row">
         <img src="https://get.wallhere.com/photo/nature-landscape-1429571.jpg" className="shadow-2xl" />
-        <h1 className='text-6xl font-bold mt-10 ml-16 text-shadow-custom'>ABOUT</h1>
+        <h1 className='text-6xl font-bold mt-10 ml-16 text-shadow-custom'>เกี่ยวกับเรา</h1>
         <p className='text-4xl mt-5 ml-16'>สถานที่ท่องเที่ยวในประเทศไทยที่น่าสนใจ</p>
         <div className='grid grid-cols-2 gap-2'>
           <div >
@@ -26,15 +26,15 @@ const About = () => {
           </div>
           <div>
             <p className='text-2xl font-meduim mt-16 ml-40'>เกาะพีพี จังหวัดกระบี่</p>
-            <img src="https://content.skyscnr.com/m/5d2172e04d2c4985/original/GettyImages-482107216.jpg?resize=1800px:1800px&quality=100" className="shadow-2xl w-3/5 ml-36 mt-5" />
+            <img src="https://content.skyscnr.com/m/5a030522220ae3aa/original/GettyImages-178744591_doc.jpg?resize=1800px:1800px&quality=100" className="shadow-2xl w-3/5 ml-36 mt-5" />
           </div>
           <div>
-            <p className='text-2xl font-meduim mt-16 ml-40'>สามพันโบก จังหวัดอุบลราชธานี</p>
-            <img src="https://content.skyscnr.com/m/5d2172e04d2c4985/original/GettyImages-482107216.jpg?resize=1800px:1800px&quality=100" className="shadow-2xl w-3/5 ml-36 mt-5" />
+            <p className='text-2xl font-meduim mt-16 ml-40'>ปราสาทหินพนมรุ้ง จังหวัดบุรีรัมย์</p>
+            <img src="https://content.skyscnr.com/m/0ac8713d45830d9e/original/GettyImages-485805928.jpg?resize=1800px:1800px&quality=100" className="shadow-2xl w-3/5 ml-36 mt-5" />
           </div>
           <div>
-            <p className='text-2xl font-meduim mt-16 ml-40'>สามพันโบก จังหวัดอุบลราชธานี</p>
-            <img src="https://content.skyscnr.com/m/5d2172e04d2c4985/original/GettyImages-482107216.jpg?resize=1800px:1800px&quality=100" className="shadow-2xl w-3/5 ml-36 mt-5" />
+            <p className='text-2xl font-meduim mt-16 ml-40'>ปราสาทหินพิมาย จังหวัดนครราชสีมา</p>
+            <img src="https://content.skyscnr.com/m/622adfce027cb90a/original/GettyImages-513780733_doc.jpg?resize=1800px:1800px&quality=100" className="shadow-2xl w-3/5 ml-36 mt-5" />
           </div>
           <div>
             <p className='text-2xl font-meduim mt-16 ml-40'>สามพันโบก จังหวัดอุบลราชธานี</p>
