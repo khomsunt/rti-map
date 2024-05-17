5adac2d6b99c809888bad71adce5e4fd94e3dc41
sureelux <sureeluxtip2545@gmail.com>
2024-05-17 14:51:10 +0700

    nt

--- src/app/ArtPage/page.tsx
+++ src/app/ArtPage/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 const ArtPage = () => {
   return (
     <div className='text-center text-3xl'>
-      AAAAAA
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="">
     <div className="hero-content flex-col lg:flex-row">
-      <img src="https://www.pngmart.com/files/11/Earth-Travel-World-PNG-Transparent-Image.png" className="shadow-2xl w-full" />
+      <img src="https://get.wallhere.com/photo/nature-landscape-mountains-trees-clouds-sky-rocks-lake-forest-Jasper-National-Park-Alberta-Canada-1907889.jpg" className="shadow-2xl w-full h-full" />
       </div>
   </div>
   );
