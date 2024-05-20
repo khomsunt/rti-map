a9cdec4d94f0bc8f9b98b7aa60813749ce56a38a
tphrmmung <tphrmmung@gmail.com>
2024-05-20 16:04:04 +0700

    ofdpkg

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,37 +1,149 @@
-import React from 'react'
+"use client";
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
-const About = () => {
-  return (
-    <div className='h-screen'>
-      <div className="hero min-h-screen bg-base-100 flex justify-center items-center">
-        <div className="max-w-screen-lg">
-          <img src="https://cms.dmpcdn.com/travel/2023/04/14/10feb5a0-da8f-11ed-b50c-375a25fe19c4_webp_original.jpg" className="w-full rounded-lg shadow-2xl mb-8 ml-32 mt-20" style={{ maxWidth: '800px' }} />
-          <h1 className='text-center text-4xl font-bold mb-4'>ที่เที่ยวสวยที่สุดในไทย มีที่ไหนบ้าง!! มาเช็คลิสต์กัน</h1>
-          <p className='text-lg text-gray-800 leading-relaxed'>
-            ในประเทศไทย บ้านเรามีที่เที่ยวเยอะแยะแบบที่ไปกันได้ทั้งปีทุกวี่ทุกวันก็ยังไม่มีหมดค่ะ 
-            นี่ยังไม่นับรวมแหล่งท่องเที่ยวใหม่ๆ ที่ผุดกันขึ้นมาเรื่อยๆ เรียกได้ว่าเมืองไทยของเรานั้น 
-            แทบจะเป็นสวรรค์ของนักท่องเที่ยวกันเลย ว่าแล้วมาดู 55 ที่เที่ยวสวยที่สุดในไทย ชาตินี้ต้องไปสักครั้ง!!
-          </p> 
-              <h1 className='text-center text-4xl font-bold mb-4 mt-5'>1. วัดพระเเก้ว กรุงเทพฯ</h1>
-                <img src="https://cms.dmpcdn.com/travel/2022/06/16/0deb8900-ed4c-11ec-809d-c172d7e1e8bc_webp_original.jpg" className="w-full rounded-lg shadow-2xl mb-8 ml-32 mt-10" style={{ maxWidth: '800px' }} />
-                <p className='text-lg text-gray-800 leading-relaxed'>
-                    วัดคู่บ้านคู่เมืองของประเทศไทย และกรุงรัตนโกสินทร์ ก็คือที่นี่ค่ะ วัดพระศรีรัตนศาสดาราม หรือ วัดพระเเก้ว 
-                    เป็นวัดในพระบรมมหาราชวังเช่นเดียวกับวัดพระศรีสรรเพชญ์ ซึ่งเป็นวัดในพระราชวังหลวงในสมัยอยุธยา
-                    นอกจากนี้ภายในวัดพระศรีรัตนศาสดารามเป็นที่ประดิษฐาน พระพุทธมหามณีรัตนปฏิมากร หรือ พระแก้วมรกต ซึ่งอัญเชิญมามาจากกรุงเวียงจันทน์ 
-                    เป็นวัดหลวงที่สำคัญในพระราชพิธีต่างๆ และยังเป็นแหล่งท่องเที่ยวที่สำคัญของประเทศไทยเราอีกด้วย
-               </p> 
-               <h1 className='text-center text-4xl font-bold mb-4 mt-5'>2. ถ้ำมรกต เกาะมุก ตรัง</h1>
-                <img src="https://cms.dmpcdn.com/travel/2022/06/16/0a696720-ed4c-11ec-8c1d-03d351db4564_webp_original.jpg" className="w-full rounded-lg shadow-2xl mb-8 ml-32 mt-10" style={{ maxWidth: '800px' }} />
-                <p className='text-lg text-gray-800 leading-relaxed'>
-                เกาะมุก ได้ชื่อว่า “ถ้ำมรกตอันล้ำค่าแห่งอันดามัน” เป็นเกาะที่ใหญ่เป็นอันดับที่ 3 ของท้องทะเลตรังเลยทีเดียว 
-                นอกจากชาดหาดที่สวยงามแล้ว เกาะมุกยังมีจุดเด่นที่ ถ้ำมรกต ถ้ำน้ำเล็กๆ ซึ่งมีความยาวประมาณ 80 เมตร
-                 นักท่องเที่ยวจะต้องลอยตัวฝ่าความมืดเพื่อเข้าไปภายในถ้ำและต้องเป็นช่วงเวลาน้ำลงเท่านั้นด้วย
-                  ภายในถ้ำน้ำทะเลจะมีสีเขียวคล้ายมรกตสวยงามอลังการ Unseen สุดๆ
-               </p> 
-        </div>
-      </div>
-    </div>
-  )
+interface Column {
+  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
 }
 
-export default About
+const columns: readonly Column[] = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toFixed(2),
+  },
+];
+
+interface Data {
+  name: string;
+  code: string;
+  population: number;
+  size: number;
+  density: number;
+}
+
+function createData(
+  name: string,
+  code: string,
+  population: number,
+  size: number,
+): Data {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+export default function StickyHeadTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '80%', overflow: 'hidden' , margin:'10%'  }}>
+      <TableContainer sx={{ maxHeight: 'calc(100vh - 190px)',overflow:'auto' }}>
+        <Table stickyHeader aria-label="sticky table ">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  sx={{
+                    minWidth: column.minWidth,
+                    backgroundColor: '#FF9999',
+                    color: column.color || '#333', // ใช้สีจากฟิลด์ color หรือสีเริ่มต้น #333
+                  }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
